e520df5630779744610a657f8691413ca84ff825
abot8639 <abot8639@gmail.com>
2026-06-03 04:17:58 +0300

    refactor: implement specific device entity classes with copyWith methods to replace generic DeviceEntity instantiation

--- lib/features/device/domain/entities/ac_device_entity.dart
+++ lib/features/device/domain/entities/ac_device_entity.dart
@@ -1,11 +1,8 @@
 import 'device_entity.dart';
 
 class AcDeviceEntity extends DeviceEntity {
-  @override
   final int? temperature;
-  @override
   final String? mode;
-  @override
   final int? coolingTime;
   final AcIrCodes acIrCodes;
 
@@ -26,53 +23,48 @@ class AcDeviceEntity extends DeviceEntity {
     this.mode,
     this.coolingTime,
     required this.acIrCodes,
-  }) : super.internal(
+  }) : super(
           type: DeviceType.airConditioner,
         );
 
-  // IR fields mapped to acIrCodes
-  @override
-  String? get irPower => acIrCodes.irPower;
-  @override
-  String? get irTempUp => acIrCodes.irTempUp;
-  @override
-  String? get irTempDown => acIrCodes.irTempDown;
-  @override
-  String? get irAuto => acIrCodes.irAuto;
-  @override
-  String? get irCool => acIrCodes.irCool;
-  @override
-  String? get irHeat => acIrCodes.irHeat;
-  @override
-  String? get irEco => acIrCodes.irEco;
-  @override
-  String? get irDry => acIrCodes.irDry;
-  @override
-  String? get irFanQuiet => acIrCodes.irFanQuiet;
-  @override
-  String? get irFanLow => acIrCodes.irFanLow;
-  @override
-  String? get irFanMed => acIrCodes.irFanMed;
-  @override
-  String? get irFanHigh => acIrCodes.irFanHigh;
-  @override
-  String? get irFanAuto => acIrCodes.irFanAuto;
-  @override
-  String? get irSwingV => acIrCodes.irSwingV;
-  @override
-  String? get irSwingH => acIrCodes.irSwingH;
-  @override
-  String? get irPlasmacluster => acIrCodes.irPlasmacluster;
-  @override
-  String? get irSuperJet => acIrCodes.irSuperJet;
-  @override
-  String? get irCoanda => acIrCodes.irCoanda;
-  @override
-  String? get irMyArea => acIrCodes.irMyArea;
-  @override
-  String? get irDisplay => acIrCodes.irDisplay;
-  @override
-  String? get irClean => acIrCodes.irClean;
+  AcDeviceEntity copyWith({
+    String? id,
+    String? name,
+    DeviceType? type,
+    bool? isOn,
+    String? roomId,
+    double? positionX,
+    double? positionY,
+    double? markerWidth,
+    double? markerHeight,
+    bool? showAsDot,
+    Object? matterNodeId = const Object(),
+    Object? matterEndpointId = const Object(),
+    Object? pin = const Object(),
+    int? temperature,
+    String? mode,
+    int? coolingTime,
+    AcIrCodes? acIrCodes,
+  }) {
+    return AcDeviceEntity(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      isOn: isOn ?? this.isOn,
+      roomId: roomId ?? this.roomId,
+      positionX: positionX ?? this.positionX,
+      positionY: positionY ?? this.positionY,
+      markerWidth: markerWidth ?? this.markerWidth,
+      markerHeight: markerHeight ?? this.markerHeight,
+      showAsDot: showAsDot ?? this.showAsDot,
+      matterNodeId: matterNodeId == const Object() ? this.matterNodeId : (matterNodeId as int?),
+      matterEndpointId: matterEndpointId == const Object() ? this.matterEndpointId : (matterEndpointId as int?),
+      pin: pin == const Object() ? this.pin : (pin as int?),
+      temperature: temperature ?? this.temperature,
+      mode: mode ?? this.mode,
+      coolingTime: coolingTime ?? this.coolingTime,
+      acIrCodes: acIrCodes ?? this.acIrCodes,
+    );
+  }
 
   @override
   List<Object?> get props => [
