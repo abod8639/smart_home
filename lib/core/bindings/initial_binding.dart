6e8e93dcad7df832fa98961fefd91372557f8124
abot8639 <abot8639@gmail.com>
2026-06-05 09:22:46 +0300

    feat: implement Google Sign-In login page and integrate firebase_auth plugin

--- lib/core/bindings/initial_binding.dart
+++ lib/core/bindings/initial_binding.dart
@@ -4,11 +4,13 @@ import 'package:smart_home/core/services/matter_service.dart';
 import 'package:smart_home/features/dashboard/presentation/controllers/dashboard_controller.dart';
 import 'package:smart_home/features/settings/presentation/controllers/settings_controller.dart';
 import 'package:smart_home/core/services/firebase_service.dart';
+import 'package:smart_home/core/services/auth_service.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(SettingsController());
+    Get.put(AuthService());
     Get.put(FirebaseService());
     Get.put(Esp32Service());
     Get.put(MatterService());
