66056ec27cb36ed31d5b7c3406f170dd6d41e15d
abot8639 <abot8639@gmail.com>
2026-06-13 09:09:34 +0300

    chore: remove obsolete Python migration scripts

--- fix_esp32_service.py
+++
@@ -1,6 +0,0 @@
-with open('lib/core/services/esp32_service.dart', 'r') as f:
-    lines = f.readlines()
-
-new_lines = ["import 'package:flutter_riverpod/flutter_riverpod.dart';\n"] + lines
-with open('lib/core/services/esp32_service.dart', 'w') as f:
-    f.writelines(new_lines)
