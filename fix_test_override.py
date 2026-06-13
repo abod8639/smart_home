5d291cedafcd2dcf61174b2b16cfe4aabda4bbf5
abot8639 <abot8639@gmail.com>
2026-06-13 09:13:22 +0300

    chore: remove automated refactor and test migration scripts

--- fix_test_override.py
+++
@@ -1,7 +0,0 @@
-with open('test/dashboard_controller_test.dart', 'r') as f:
-    c = f.read()
-
-c = c.replace('.overrideWith((_) => mockFirebase)', '.overrideWith(() => mockFirebase)')
-
-with open('test/dashboard_controller_test.dart', 'w') as f:
-    f.write(c)
