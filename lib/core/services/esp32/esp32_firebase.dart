989ae90141d77a22f0519e3e50e54e0300af179c
abot8639 <abot8639@gmail.com>
2026-06-13 08:30:54 +0300

    refactor: migrate tests from GetX to Riverpod and update ESP32 service and auth binding logic

--- lib/core/services/esp32/esp32_firebase.dart
+++ lib/core/services/esp32/esp32_firebase.dart
@@ -3,28 +3,28 @@ part of '../esp32_service.dart';
 extension Esp32Firebase on Esp32Service {
   void _initFirebaseSync() {
     _firebaseSubscriptions.add(_firebase.temperatureStream.listen((event) {
-      if (!isConnected.value && event.snapshot.value != null) {
+      if (!isConnected && event.snapshot.value != null) {
         final double temp = (event.snapshot.value as num).toDouble();
         _syncSensorsWithControllers({'temperature': temp});
       }
     }));
 
     _firebaseSubscriptions.add(_firebase.humidityStream.listen((event) {
-      if (!isConnected.value && event.snapshot.value != null) {
+      if (!isConnected && event.snapshot.value != null) {
         final double hum = (event.snapshot.value as num).toDouble();
         _syncSensorsWithControllers({'humidity': hum});
       }
     }));
 
     _firebaseSubscriptions.add(_firebase.targetTempStream.listen((event) {
-      if (!isConnected.value && event.snapshot.value != null) {
+      if (!isConnected && event.snapshot.value != null) {
         final int target = (event.snapshot.value as num).toInt();
         _syncStateWithControllers({'target_temperature': target});
       }
     }));
 
     _firebaseSubscriptions.add(_firebase.pinsStream.listen((event) {
-      if (!isConnected.value && event.snapshot.value != null) {
+      if (!isConnected && event.snapshot.value != null) {
         try {
           final Map<dynamic, dynamic> pinsMap = event.snapshot.value as Map<dynamic, dynamic>;
           final Map<String, dynamic> formattedPins = {};
