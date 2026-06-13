b24c8415c65544c194588414ceaaefb00f774e66
abot8639 <abot8639@gmail.com>
2026-06-13 09:56:05 +0300

    refactor: provide FirebaseAuth via Riverpod and update settings UI to observe hub reachability status

--- lib/core/services/auth_service.g.dart
+++ lib/core/services/auth_service.g.dart
@@ -9,6 +9,47 @@ part of 'auth_service.dart';
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint, type=warning
 
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = FirebaseAuthProvider._();
+
+final class FirebaseAuthProvider
+    extends $FunctionalProvider<FirebaseAuth, FirebaseAuth, FirebaseAuth>
+    with $Provider<FirebaseAuth> {
+  FirebaseAuthProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'firebaseAuthProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$firebaseAuthHash();
+
+  @$internal
+  @override
+  $ProviderElement<FirebaseAuth> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  FirebaseAuth create(Ref ref) {
+    return firebaseAuth(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(FirebaseAuth value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<FirebaseAuth>(value),
+    );
+  }
+}
+
+String _$firebaseAuthHash() => r'8c3e9d11b27110ca96130356b5ef4d5d34a5ffc2';
+
 @ProviderFor(AuthService)
 final authServiceProvider = AuthServiceProvider._();
 
@@ -40,7 +81,7 @@ final class AuthServiceProvider extends $NotifierProvider<AuthService, void> {
   }
 }
 
-String _$authServiceHash() => r'6eaab73f86605e39cc524bcf7c9d039e24d08209';
+String _$authServiceHash() => r'609cffe12b300f484a39f928e50a112cf0265334';
 
 abstract class _$AuthService extends $Notifier<void> {
   void build();
