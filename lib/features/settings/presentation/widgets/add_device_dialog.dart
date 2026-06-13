24b9a6832bd2378b63df38cf121ab2fb94658048
abot8639 <abot8639@gmail.com>
2026-06-13 09:00:34 +0300

    refactor: remove unused dependencies, clean up test code, and simplify string formatting in UI widgets

--- lib/features/settings/presentation/widgets/add_device_dialog.dart
+++ lib/features/settings/presentation/widgets/add_device_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:smart_home/core/theme/app_theme.dart';
 import 'package:smart_home/features/device/domain/entities/device_entity.dart';
 import 'package:smart_home/features/dashboard/presentation/controllers/dashboard_controller.dart';
