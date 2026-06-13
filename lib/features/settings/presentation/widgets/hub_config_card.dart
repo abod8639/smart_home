aa307ec0fd179b22266b3fa9f8c24abb98e6db4b
abot8639 <abot8639@gmail.com>
2026-06-13 09:01:07 +0300

    refactor: clean up unused imports and redundant mock type casting in tests and UI components

--- lib/features/settings/presentation/widgets/hub_config_card.dart
+++ lib/features/settings/presentation/widgets/hub_config_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:smart_home/core/theme/app_theme.dart';
 import 'package:smart_home/core/widgets/glass_container.dart';
 import 'package:smart_home/core/utils/responsive.dart';
