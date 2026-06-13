a3c972d50e4c242e95d7d6f04ddeee42606e7b7a
abot8639 <abot8639@gmail.com>
2026-06-13 09:10:51 +0300

    refactor: remove legacy migration scripts after successfully transitioning to Riverpod and GoRouter

--- fix_init_firebase2.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('lib/features/dashboard/presentation/controllers/dashboard_controller.dart', 'r') as f:
-    c = f.read()
-
-# I will just replace the whole `void _initFirebaseListeners() {` and whatever is after it down to the next `void ` or end of file
-c = re.sub(r'void _initFirebaseListeners\(\)\s*\{.*?\}\s*\}\s*\}\s*', 'void _initFirebaseListeners() {}\n', c, flags=re.DOTALL)
-
-with open('lib/features/dashboard/presentation/controllers/dashboard_controller.dart', 'w') as f:
-    f.write(c)
