f4ed59f9c7d49d629c018740bc23bd7028abb592
abot8639 <abot8639@gmail.com>
2026-06-14 09:55:50 +0300

    docs: improve class documentation and refactor IR code deserialization to use IrCodeModel

--- lib/features/device/domain/usecases/save_devices_usecase.dart
+++ lib/features/device/domain/usecases/save_devices_usecase.dart
@@ -5,16 +5,21 @@ import '../../data/repositories/device_repository_impl.dart';
 
 part 'save_devices_usecase.g.dart';
 
+/// Use case to save/persist a list of smart home devices.
 class SaveDevicesUseCase {
+  /// The abstract device repository interface.
   final DeviceRepository repository;
 
+  /// Creates a [SaveDevicesUseCase] instance with [repository].
   SaveDevicesUseCase(this.repository);
 
+  /// Executes the use case to save the provided [devices].
   Future<void> call(List<DeviceEntity> devices) async {
     await repository.saveDevices(devices);
   }
 }
 
+/// Riverpod provider for [SaveDevicesUseCase].
 @riverpod
 SaveDevicesUseCase saveDevicesUseCase(Ref ref) {
   return SaveDevicesUseCase(ref.watch(deviceRepositoryProvider));
