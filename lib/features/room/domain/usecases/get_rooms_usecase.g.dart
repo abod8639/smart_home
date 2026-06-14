027daef29f426b66b1940ec42a119201f3531190
abot8639 <abot8639@gmail.com>
2026-06-14 05:50:13 +0300

    feat: implement SaveRoomsUseCase and update repositories and tests to use RoomModel instead of RoomEntity

---
+++ lib/features/room/domain/usecases/get_rooms_usecase.g.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_rooms_usecase.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(getRoomsUseCase)
+final getRoomsUseCaseProvider = GetRoomsUseCaseProvider._();
+
+final class GetRoomsUseCaseProvider
+    extends
+        $FunctionalProvider<GetRoomsUseCase, GetRoomsUseCase, GetRoomsUseCase>
+    with $Provider<GetRoomsUseCase> {
+  GetRoomsUseCaseProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'getRoomsUseCaseProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$getRoomsUseCaseHash();
+
+  @$internal
+  @override
+  $ProviderElement<GetRoomsUseCase> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  GetRoomsUseCase create(Ref ref) {
+    return getRoomsUseCase(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(GetRoomsUseCase value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<GetRoomsUseCase>(value),
+    );
+  }
+}
+
+String _$getRoomsUseCaseHash() => r'1ad5675c72a123c49436834ff97291a016468ce1';
