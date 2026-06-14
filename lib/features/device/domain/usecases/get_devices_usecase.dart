53d3a573984d8138a8bea9b761c6cabb66a59bdc
abot8639 <abot8639@gmail.com>
2026-06-14 09:55:40 +0300

    docs: add comprehensive documentation to device cards and entities and refactor IR code deserialization to use models

--- lib/features/device/domain/usecases/get_devices_usecase.dart
+++ lib/features/device/domain/usecases/get_devices_usecase.dart
@@ -5,16 +5,21 @@ import '../../data/repositories/device_repository_impl.dart';
 
 part 'get_devices_usecase.g.dart';
 
+/// Use case to retrieve all smart home devices.
 class GetDevicesUseCase {
+  /// The abstract device repository interface.
   final DeviceRepository repository;
 
+  /// Creates a [GetDevicesUseCase] instance with [repository].
   GetDevicesUseCase(this.repository);
 
+  /// Executes the use case to load all devices.
   Future<List<DeviceEntity>> call() async {
     return await repository.getDevices();
   }
 }
 
+/// Riverpod provider for [GetDevicesUseCase].
 @riverpod
 GetDevicesUseCase getDevicesUseCase(Ref ref) {
   return GetDevicesUseCase(ref.watch(deviceRepositoryProvider));
