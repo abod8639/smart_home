00181f69797d4fee3ba0630c47ae314eda1340ba
abot8639 <abot8639@gmail.com>
2026-06-14 05:48:45 +0300

    feat: implement Room repository providers and use cases with Riverpod generation

---
+++ lib/features/device/domain/usecases/save_devices_usecase.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'save_devices_usecase.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(saveDevicesUseCase)
+final saveDevicesUseCaseProvider = SaveDevicesUseCaseProvider._();
+
+final class SaveDevicesUseCaseProvider
+    extends
+        $FunctionalProvider<
+          SaveDevicesUseCase,
+          SaveDevicesUseCase,
+          SaveDevicesUseCase
+        >
+    with $Provider<SaveDevicesUseCase> {
+  SaveDevicesUseCaseProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'saveDevicesUseCaseProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$saveDevicesUseCaseHash();
+
+  @$internal
+  @override
+  $ProviderElement<SaveDevicesUseCase> $createElement(
+    $ProviderPointer pointer,
+  ) => $ProviderElement(pointer);
+
+  @override
+  SaveDevicesUseCase create(Ref ref) {
+    return saveDevicesUseCase(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(SaveDevicesUseCase value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<SaveDevicesUseCase>(value),
+    );
+  }
+}
+
+String _$saveDevicesUseCaseHash() =>
+    r'ab1ea4db49f28a4c6f2be978992c72724ce99fc7';
