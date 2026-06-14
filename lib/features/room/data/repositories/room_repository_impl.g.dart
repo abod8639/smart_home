b9bd8d5f2c3f81bcc5dec275ee7ce264eec5c27e
abot8639 <abot8639@gmail.com>
2026-06-14 05:49:43 +0300

    feat: implement room retrieval and storage use cases and update repository tests to use data models

---
+++ lib/features/room/data/repositories/room_repository_impl.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'room_repository_impl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(roomRepository)
+final roomRepositoryProvider = RoomRepositoryProvider._();
+
+final class RoomRepositoryProvider
+    extends $FunctionalProvider<RoomRepository, RoomRepository, RoomRepository>
+    with $Provider<RoomRepository> {
+  RoomRepositoryProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'roomRepositoryProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$roomRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<RoomRepository> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  RoomRepository create(Ref ref) {
+    return roomRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(RoomRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<RoomRepository>(value),
+    );
+  }
+}
+
+String _$roomRepositoryHash() => r'ef3765ad9ae68f88cff97e127dd2358db863ad11';
