1640a093a4e0616a3f64af9a5005e1eff2fb922c
abot8639 <abot8639@gmail.com>
2026-06-14 05:57:14 +0300

    docs: add documentation to room domain and presentation layer components while migrating to RoomPlacementPage

--- lib/features/room/domain/repositories/room_repository.dart
+++ lib/features/room/domain/repositories/room_repository.dart
@@ -1,8 +1,16 @@
 import '../entities/room_entity.dart';
 
+/// Repository interface defining room data access contracts.
 abstract class RoomRepository {
+  /// Retrieves all rooms from the data source.
   Future<List<RoomEntity>> getRooms();
+
+  /// Adds a new room.
   Future<void> addRoom(RoomEntity room);
+
+  /// Deletes a room by its [id].
   Future<void> deleteRoom(String id);
+
+  /// Persists a list of [rooms], overwriting the existing ones.
   Future<void> saveRooms(List<RoomEntity> rooms);
 }
